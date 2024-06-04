b838ea60033032674bc74c7bca7a0a81669f727a
Muhammed Tijjani <ahmedmuhammedt@gmail.com>
2024-06-04 12:19:12 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <body
           className={`${montserrat.variable} ${manrope.className} ${mansalva.variable} ${roboto.variable} ${inter.variable}`}
         >
-          {children}
+          {/* {children} */}
 
           <NewTemplateModal />
           <Toaster />
