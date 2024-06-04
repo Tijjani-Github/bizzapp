b1fed87fa9a33324df13c5093b56f72361957fc3
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-06-04 10:12:47 +0100

    agent dashboard

--- src/app/(dashboards)/agent/dashboard/page.tsx
+++ src/app/(dashboards)/agent/dashboard/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { CustomerTable } from "../../admin/customers/customtable";
+import { AgentCase } from "@/app/(components)/agentcasePage";
+import { Closed } from "../resolved/resolved";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="flex w-full">
+        <div className="w-1/2">
+          <AgentCase />
+        </div>
+        <div className="w-1/2">
+          <Closed />
+        </div>
+      </div>
+      <CustomerTable />
+    </>
+  );
 };
 
 export default page;
